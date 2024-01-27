7595cf26c9222aeabaf6d7e58ffce9e8ed983afc
Zakky <zakkyrahman1131@gmail.com>
2024-01-27 09:58:31 +0700

    #1

---
+++ php/smkrevit/index.php
@@ -0,0 +1,21 @@
+<?php 
+
+    echo "BELAJAR PHP";
+
+    echo "<br>";
+
+    echo "Saya belajar PHP";
+
+    echo '<h1 style="background-color:purple">Saya sedang belajar PHP</h1>
+        <p style="background-color:pink">Saya sedang bermain</p>
+    ';
+
+    echo 'Saya belajar'.'PHP'.'<br>';
+
+    echo 'tahun 2020';
+
+    echo "<br>";
+
+    echo 2000 + 7;
+
+?>
