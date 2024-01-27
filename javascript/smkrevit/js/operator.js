445ec76df4e4a4d03506782a80fc67fd6c9f59d7
Zakky <zakkyrahman1131@gmail.com>
2024-01-27 10:25:23 +0700

    yt#4

---
+++ javascript/smkrevit/js/operator.js
@@ -0,0 +1,41 @@
+let x = 2;
+x = -x;
+console.log(x);
+
+let p, l, t, volume;
+p = 5;
+l = 6
+t = 7;
+volume = p * l * t;
+console.log(volume);
+
+console.log(2 + 3);
+console.log(2 - 3);
+console.log(2 * 3);
+console.log(3 % 3);
+console.log(3 / 3);
+
+let kota ="sido" + "arjo";
+console.log(kota);
+
+console.log("1" + 5 + 6);
+console.log(1 + 5 + "6");
+
+console.log(2 ** 2);
+console.log(4 ** 2);
+console.log(9 ** 2);
+
+let a, b, c;
+a = b = c = 3 + 3;
+console.log(a);
+
+let inc = 4;
+inc++;
+console.log(inc);
+
+inc--;
+console.log(inc);
+
+let dec = 4;
+dec--;
+console.log(dec);
